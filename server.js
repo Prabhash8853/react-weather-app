64f5a620ea98696a5de565ae555f2f25e2c56d38
Prabhash8853 <30629586+Prabhash8853@users.noreply.github.com>
2018-11-04 15:38:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.static('public'));
 
-app.liste(PORT, function(){
+app.listen(PORT, function(){
     console.log('express server is up on port' + PORT);
 });
